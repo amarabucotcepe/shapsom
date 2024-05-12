485458aaba7a211ef9ebd1565dbefadc728ee023
Alexandre Rodolfo <armp@ecomp.poli.br>
2024-05-12 01:30:09 -0300

    Mapa SOM com parâmetros na tela inicial

--- Página_Inicial.py
+++ Página_Inicial.py
@@ -6,66 +6,135 @@ from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import globals
 from som import rodar_algoritmo
+import altair as alt
+from my_utils import create_map, HEX_SHAPE
+import numpy as np
 
 
 st.title("ShapSom 🤖")
 st.subheader("Análise de agrupamento de dados")
-
 title = st.text_input("Título do relatório", help='Escolha o título do relatório')
-
 tipo = st.radio('Tipo de arquivo',['csv','excel'], help='Escolha o tipo de arquivo. csv: separado por vírgula, excel: planilha excel')
 
-if tipo == 'csv':
-    download_file = 'modelo.csv'
-else:
-    download_file = 'modelo.xslx'
+download_file = 'modelo.csv' if tipo == 'csv' else 'modelo.xslx'
 
 with st.expander("Precisa do modelo?", expanded=False):
     st.download_button('Modelo', 'modelo', file_name=download_file, help='Modelo de planilha a ser enviada')
 
 file = st.file_uploader("Faça upload do seu arquivo", type=['csv'], help='Se já preencheu os dados na planilha modelo faça upload de um arquivo csv ou excel, ou faça o download do modelo e preencha com seus dados')
 
-
 if file is not None:
- 
-    if tipo == 'csv':
-        #checar qual sep está sendo usado
-        df = pd.read_csv(file, sep=',')
-    else:
-        df = pd.read_excel(file)
-    # st.dataframe(df)
-    
+    df = pd.read_csv(file, sep=',') if tipo == 'csv' else pd.read_excel(file)
     globals.current_database = df.dropna()
     globals.current_database_name = file.name.split(".")[0]
-
     string_list = df.columns.tolist()
-
     st.divider()
 
     st.subheader("Selecione as colunas")
     with st.container():
         col1, col2, col3, col4 = st.columns(4)
-
         with col1:
-            globals.current_label_columns = st.multiselect("Nome", string_list)
+            others = []
+            globals.current_label_columns = st.multiselect("Nome", [col for col in string_list if col not in others], max_selections=1)
         with col2:
-            options = [col for col in string_list if col not in globals.current_label_columns]
-            globals.current_input_columns = st.multiselect("Entradas", [col for col in string_list if col not in globals.current_label_columns])
+            others = globals.current_label_columns
+            globals.current_input_columns = st.multiselect("Entradas", [col for col in string_list if col not in others])
         with col3:
-            globals.current_output_columns = st.multiselect("Saída", [col for col in string_list if col not in globals.current_label_columns and col not in globals.current_input_columns])
+            others = globals.current_label_columns + globals.current_input_columns
+            globals.current_output_columns = st.multiselect("Saída", [col for col in string_list if col not in others], max_selections=1)
         with col4:
-            globals.current_hidden_columns = st.multiselect("Ocultar", [col for col in string_list if col not in globals.current_label_columns and col not in globals.current_input_columns and col not in globals.current_output_columns])
+            others = globals.current_label_columns + globals.current_input_columns + globals.current_output_columns
+            globals.current_hidden_columns = st.multiselect("Ocultar", [col for col in string_list if col not in others])
+
+    choose_columns = st.button("Pronto")
+    if choose_columns:
+        globals.som_chart = None
+        globals.file_uploaded_start_flag = True
+
+        numeric_cols = list(globals.current_database.select_dtypes(include=['float64', 'int64']).columns)
+        crunched_cols = globals.current_label_columns + numeric_cols
+        crunched_df = globals.current_database[crunched_cols]
+        municipio_dfs = crunched_df.groupby(globals.current_label_columns[0])
+        list_of_dfs = [group_df for _, group_df in municipio_dfs]
+        new_df = []
+        for l in list_of_dfs:
+            new_data = [l[globals.current_label_columns[0]].values[0]] + np.array(np.average(l[numeric_cols].values, axis=0)).astype(np.float64).flatten().tolist()
+            new_df.append(new_data)
+        ##################################################################################################################################
+        globals.crunched_df = pd.DataFrame(new_df, columns=crunched_cols) # OU ISSO AQUI É A BASE QUE VAI PRO SOM TIRANDO MÉDIA DE UBS!!!#
+        ##################################################################################################################################
 
     selected_df = df.drop(columns=globals.current_hidden_columns)
 
     st.divider()
 
+    if globals.file_uploaded_start_flag or globals.som_chart is None:
+        globals.som_chart = alt.Chart(pd.DataFrame([], columns=["x","y","Nota"])).mark_point(filled=True, shape=HEX_SHAPE).encode(
+            x=alt.X('x', scale=alt.Scale(domain=(0,30))),
+            y=alt.Y('y', scale=alt.Scale(domain=(0,30))),
+            size=alt.Size('Nota', scale=alt.Scale(domain=(0,1))),
+            color=alt.Color("Cor:N", scale=None)
+        ).interactive().configure_view(fill='black').properties(width=400,height=400, title="Mapa SOM (Aguardando dados...)")
+        globals.som = st.altair_chart(globals.som_chart, use_container_width=True)
+    else:
+        globals.som = st.altair_chart(globals.som_chart, use_container_width=True)
+
     with st.expander("Parâmetros SOM", expanded=False):
-        globals.cluster_distance = st.slider("Cluster Distance", min_value=0.5, max_value=4.0, value=1.0, step=0.1, help='Distancia entre os grupos')
-        globals.epochs = st.slider("Epochs", min_value=2, max_value=5, value=3, step=1)  # Changed step to int
-        globals.size = st.slider("Size", min_value=5, max_value=60, value=30, step=1)  # Changed step to int
-        globals.sigma = st.slider("Sigma", min_value=1, max_value=10, value=9, step=1)  # Changed step to int
-        globals.lr = st.slider("Learning Rate", min_value=-3.0, max_value=-1.0, value=-2.0, step=0.1)
+        globals.sigma = st.slider("Sigma", min_value=1, max_value=10, value=9, help="A largura da vizinhança inicial no mapa SOM. Controla a extensão das alterações que ocorrem durante o treinamento. Um valor alto significa que mais neurônios serão influenciados durante o treinamento inicial, enquanto um valor baixo resultará em um ajuste mais fino.")
+        globals.size = st.slider("Tamanho do mapa", min_value=5, max_value=50, value=30, help="O tamanho do mapa SOM, especificado pelo número total de neurônios (unidades). Mapas maiores podem representar características complexas com maior precisão, mas também requerem mais tempo de treinamento.")
+        globals.lr = st.slider("Taxa de aprendizado", min_value=-5.0, max_value=-1.0, value=-3.0, step=0.25, help="Taxa de aprendizado inicial. Controla a velocidade de adaptação do mapa durante o treinamento. Valores muito altos podem levar a uma convergência instável, enquanto valores muito baixos podem resultar em um treinamento lento.")
+        globals.epochs = st.slider("Épocas", min_value=100, max_value=30000, step=100, value=10000, help="Número de épocas (iterações) de treinamento. O número de vezes que o mapa será treinado em relação aos dados de entrada. Mais épocas geralmente resultam em um mapa mais bem ajustado, mas também aumentam o tempo de treinamento.")
+        globals.cluster_distance = st.slider("Distância dos agrupamentos", min_value=0.5, max_value=3.0, step=0.25, value=1.5, help="A distância mínima entre agrupamentos de neurônios para considerar a formação de grupos distintos. Valores mais altos podem resultar em agrupamentos mais distintos, enquanto valores mais baixos podem mesclar grupos semelhantes.")
+        globals.topology = st.radio("Topologia", options=["Retangular", "Hexagonal"], index=1, help="Topologia do mapa SOM para formação de vizinhanças.")
+        globals.output_influences = st.radio("Coluna de saída influencia nos resultados (experimental)", options=["Sim", "Não"], index=0, help="Se a coluna de saída dos dados de entrada influencia nos resultados finais. Selecione 'Sim' para permitir que a coluna de saída tenha impacto na organização do mapa, ou 'Não' para desconsiderar a coluna de saída durante o treinamento.")
+        update_map = st.button("Alterar parâmetros")
+
+    has_enough_data = globals.current_label_columns and globals.current_output_columns and len(globals.current_input_columns) >= 2
+    if (update_map or globals.file_uploaded_start_flag) and has_enough_data:
+        globals.file_uploaded_start_flag = False
+        som_iter = create_map(
+            globals.crunched_df,
+            cluster_distance=globals.cluster_distance,
+            lr=10**globals.lr,
+            epochs=globals.epochs,
+            size=globals.size,
+            sigma=globals.sigma,
+            label_column=globals.current_label_columns[0],
+            output_column=globals.current_output_columns[0],
+            variable_columns=globals.current_input_columns,
+            interval_epochs=globals.epochs//20,
+            output_influences=globals.output_influences == "Sim",
+            topology="rectangular" if globals.topology == "Retangular" else "hexagonal"
+        )
+
+        for i,som_data in enumerate(som_iter):
+            load_percentage = round((i+1) * (globals.epochs//20) * 100 / globals.epochs)
+            chart_title = f"Mapa SOM ({load_percentage}%)" if load_percentage < 100 else "Mapa SOM"
+            chart_data = som_data
+            chart_data.columns = ["Municípios", "Nota", "x", "y", "Cor", "Grupo"]
+            ###################################################################################
+            globals.som_data = chart_data # OU USA ISSO AQUI PRA PEGAR OS RESULTADOS DO SOM!!!#
+            ###################################################################################
+
+            if globals.topology == "Retangular":
+                c = alt.Chart(globals.som_data).mark_square(filled=True).encode(
+                    x="x",
+                    y="y",
+                    size="Nota",
+                    color=alt.Color("Cor:N", scale=None),
+                    tooltip=["Grupo", "Nota", "Municípios"]
+                ).interactive().configure_view(fill='black').properties(width=400, height=400, title=chart_title)
+            else:
+                c = alt.Chart(globals.som_data).mark_point(filled=True, shape=HEX_SHAPE).encode(
+                    x="x",
+                    y="y",
+                    size="Nota",
+                    color=alt.Color("Cor:N", scale=None),
+                    tooltip=["Grupo", "Nota", "Municípios"]
+                ).interactive().configure_view(fill='black').properties(width=400, height=400, title=chart_title)
+
+            globals.som_chart = c
+            globals.som.altair_chart(globals.som_chart, use_container_width=True)
     
     global use_shap
     globals.use_shap = st.checkbox("Criar SHAP",help='Selecione para obter análise completa dos dados')
@@ -75,3 +144,6 @@ if file is not None:
     if submit_button:
         st.dataframe(selected_df)
         rodar_algoritmo()
+else:
+    globals.file_uploaded_start_flag = False
+    globals.som_chart = None
---
+++ my_utils.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import numpy as np
+import random
+import string
+from minisom import MiniSom
+from scipy.cluster.hierarchy import linkage, fcluster
+import colorsys
+import warnings
+warnings.filterwarnings("ignore")
+
+HEX_SHAPE = "M0,-1 L0.866,-0.5 L0.866,0.5 L0,1 L-0.866,0.5 L-0.866,-0.5 Z"
+
+def cluster_coordinates(coordenadas: 'list[tuple]', distancia_maxima: float) -> 'list[list[tuple]]':
+    '''Recebe uma lista de coordenadas e devolve elas agrupadas de acordo com a distância definida'''
+    mat = linkage(coordenadas, method='single', metric='chebyshev')
+    cluster_ids = fcluster(mat, distancia_maxima, criterion='distance')
+    elements_with_id = lambda id : np.array(np.where(cluster_ids == id), dtype=int).flatten().tolist()
+    clusters = [[coordenadas[i] for i in elements_with_id(id)] for id in set(cluster_ids)]
+    clusters = sorted(clusters, key=len, reverse=True)
+    return clusters
+
+def hsv_to_hex(hsv) -> str:
+    h, s, v = hsv
+    r, g, b = colorsys.hsv_to_rgb(h / 360, s, v)
+    r,g,b = int(r * 255), int(g * 255), int(b * 255)
+    hex_color = f"#{r:02x}{g:02x}{b:02x}"
+    return hex_color
+
+def random_string(N: int) -> str:
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=N))
+
+def normalize(data: np.ndarray) -> np.ndarray:
+    data = (data if isinstance(data, np.ndarray) else np.array(data)).astype(np.float64)
+    data -= data.min()
+    data /= data.max()
+    return data
+
+def get_som_data(som: MiniSom, labels, x, y, cluster_distance) -> pd.DataFrame:
+    units = som.labels_map(x, labels)
+    is_hex = som.topology == "hexagonal"
+    units = {((_x + 0.5) if (is_hex and _y % 2 == 1) else (_x), _y) : units[_x,_y] for _x,_y in units.keys()}
+    clusters = cluster_coordinates(list(units.keys()), cluster_distance)
+    
+    cluster_amount = len(clusters)
+    hsv_boundaries = ((np.arange(cluster_amount+1) / (cluster_amount+1)) * 360).flatten().tolist()
+    hsv_region_width = (360 / (cluster_amount+1)) * 0.25
+    hsv_centers = np.array([(hsv_boundaries[i] + hsv_boundaries[i+1]) / 2 for i in range(cluster_amount)]).flatten().tolist()
+    hsv_regions = np.array([(c-(hsv_region_width/2), c+(hsv_region_width/2)) for c in hsv_centers]).tolist()
+    som_data_list = []
+
+    for i, coords in enumerate(clusters):
+        coord_dict = {}
+        cell_scores = []
+        for c in filter(lambda _c : units[_c], coords):
+            cell_labels = list(units[c])
+            score_data = np.array([y.loc[y['label'] == u, y.columns[-1]].values[0] for u in cell_labels]).flatten()
+            variables = [x[list(labels).index(v)] for v in units[c]]
+            cell_score = np.average(score_data)
+            cell_scores.append(cell_score)
+            coord_dict[c] = {"labels": cell_labels, "scores": score_data, "variables": variables, "cell_score": cell_score}
+        
+        min_hsv, max_hsv = hsv_regions[i]
+        min_score, max_score = min(cell_scores), max(cell_scores)
+        for c in coord_dict.keys():
+            _labels = ", ".join(coord_dict[c]["labels"])
+            _score = coord_dict[c]["cell_score"]
+            _x = c[0]
+            _y = c[1]
+            _color_percent = 0.5 if (min_score == max_score) else (_score - min_score) / (max_score - min_score)
+            _hue_hsv = (max_hsv * _color_percent) + (min_hsv * (1 - _color_percent))
+            _color_hex = hsv_to_hex([_hue_hsv, 1, 1])
+            som_data_list.append([_labels, round(_score, 2), _x, _y, _color_hex, i+1])
+    
+    som_data = pd.DataFrame(som_data_list, columns=["labels", "score", "x", "y", "color", "cluster"])
+    return som_data
+
+def create_map(df: pd.DataFrame, label_column: str, variable_columns: 'list[str]', output_column: str, size: int = 50, lr: float = 1e-1, epochs: int = 1000, sigma = 2, topology = "hexagonal", cluster_distance: float = 2, interval_epochs: int=100, output_influences=True):
+    labels = df[label_column]
+
+    x = np.array(df[variable_columns].select_dtypes(include='number').values)
+    if output_influences:
+        val_multipliers = df[output_column].values
+        new_x = []
+        for r,m in zip(x, val_multipliers):
+            new_r = r if np.sum(r) != 0 else np.ones_like(r)
+            new_r /= np.sum(new_r)
+            new_x.append(new_r * m)
+        x = np.array(new_x)
+        
+    y = pd.concat([pd.DataFrame({"label": labels}), df[output_column]], axis=1)
+    som = MiniSom(
+        x=size,
+        y=size,
+        input_len=len(x[0]),
+        sigma=sigma,
+        topology=topology,
+        learning_rate=lr,
+        neighborhood_function="gaussian",
+        activation_distance="euclidean"
+    )
+    
+    som.pca_weights_init(x)
+    for _ in range(epochs // interval_epochs):
+        som.train_random(x, interval_epochs, verbose=False)
+        som_data = get_som_data(som, labels, x, y, cluster_distance)
+        yield som_data
