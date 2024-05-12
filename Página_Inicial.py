6e9eed3e3f3c28092af4a61238f26acd8371bfcd
RiquelmeLopes <riqueque2010@hotmail.com>
2024-05-11 23:44:46 -0300

    Imports e arquivos não utilizados retirados

--- Página_Inicial.py
+++ Página_Inicial.py
@@ -1,66 +1,12 @@
 import streamlit as st
-
-from scipy.cluster.hierarchy import linkage, fcluster
-from colorsys import hsv_to_rgb
-from minisom import MiniSom
-import numpy as np
 import pandas as pd
-import os
-import re
-import io
-import matplotlib.pyplot as plt
-from xgboost import XGBRegressor
-from tqdm import tqdm
-import shap
 import warnings
 warnings.filterwarnings("ignore")
-from datetime import datetime
-import json
-import folium
-import shutil
-from copy import deepcopy
-from sklearn.preprocessing import MinMaxScaler
-from difflib import SequenceMatcher
-import statistics
-import math
-from sklearn.ensemble import IsolationForest
-
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
-from reportlab.lib.utils import ImageReader
-from reportlab.lib.units import inch
-from reportlab.pdfgen import canvas
-import PIL
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-from reportlab.platypus.flowables import Image
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.platypus import Paragraph
-from unidecode import unidecode
-import imgkit
-
-import matplotlib as mpl
-from pypdf import PdfMerger
-#from globals import selected_df, cluster_distance, epochs, size, sigma,lr, use_shap, current_database, current_database_name, current_output_columns, current_hidden_columns, current_input_columns, current_label_columns
 import globals
 from som import rodar_algoritmo
-# from shap import 
-from report import (
-documento_1,
-documento_2_2_e_2_4,
-documento_2_3,
-documento_2_5,
-documento_2_6,
-documento_7_1,
-salvar_pdfs,
-add_cabecalho,
-gerar_anexos
-)
 
-"""### Anexos"""
-
-gerar_anexos()
 
 st.title("ShapSom 🤖")
 st.subheader("Análise de agrupamento de dados")
@@ -114,12 +60,6 @@ if file is not None:
 
     st.divider()
 
-    # st.subheader("Selecione os dados de interesse")
-    # if df[saida].min() is not None:
-    #     triagem = st.slider("Triagem", min_value=df[saida].min(), max_value=df[saida].max(), value=(df[saida].min(), df[saida].max()), step=0.1)
-    #     selected_df = selected_df.loc[(selected_df[saida] > triagem[0]) & (df[saida] < triagem[1])]
-    
-
     with st.expander("Parâmetros SOM", expanded=False):
         globals.cluster_distance = st.slider("Cluster Distance", min_value=0.5, max_value=4.0, value=1.0, step=0.1, help='Distancia entre os grupos')
         globals.epochs = st.slider("Epochs", min_value=2, max_value=5, value=3, step=1)  # Changed step to int
@@ -132,16 +72,6 @@ if file is not None:
 
     submit_button = st.button('Executar')
     
-    
     if submit_button:
         st.dataframe(selected_df)
         rodar_algoritmo()
-        # documento_1()
-        # documento_2_2_e_2_4()
-        # documento_2_3()
-        # documento_2_5()
-        # documento_2_6()
-        # documento_7_1()
-        # salvar_pdfs()
-        # add_cabecalho(title)
-        # gerar_anexos()
