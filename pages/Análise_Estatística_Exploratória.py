a09677c81676517ba5c3378b80ac2789357ae369
RiquelmeLopes <riqueque2010@hotmail.com>
2024-05-28 18:02:59 -0300

    Textos da seção de Análise Estatística

--- pages/Análise_Estatística_Exploratória.py
+++ pages/Análise_Estatística_Exploratória.py
@@ -68,7 +68,19 @@ with st.spinner('Gerando mapa...'):
       components.html(m_repr_html_, height=600)
     else:
         generate_map()
+st.info('Figura 1 - Mapa do Estado de Pernambuco')
 
+st.markdown(''' A figura 1 exibe uma análise geoespacial das notas de proficiência dos estudantes,  no âmbito da educação básica. As diferentes tonalidades de cores no mapa indicam variações nos níveis de proficiência entre os diversos municípios. A imagem, portanto, oferece uma visão detalhada e regionalizada do desempenho educacional no estado de Pernambuco, evidenciando as disparidades entre diferentes municípios.
+Este tipo de visualização é essencial para identificar regiões que necessitam de intervenções educacionais mais intensivas e ajuda a entender as disparidades educacionais entre diferentes regiões, contribuindo para políticas públicas mais informadas e direcionadas. 
+''')
+
+with st.container(border=True) as container1:
+    st.markdown(''' **Interpretação do Mapa** \n
+        Tonalidades de Cores: \n
+        -Cores Vermelhas: Indicam áreas com menores notas de proficiência, sugerindo um desempenho educacional mais baixo.\n
+        -Cores Azuis: Representam áreas com maiores notas de proficiência, indicando um desempenho educacional superior.\n
+        -Cores Mais Escuras: Correspondem aos extremos dos níveis de proficiência, onde as cores vermelhas mais escuras representam os piores desempenhos e as cores azuis mais escuras representam os melhores desempenhos.\n''')
+    
 st.info(f'Município x {df.columns[-1]}', icon='🌎')
 
 st.subheader('Análise Estatística')
@@ -85,13 +97,14 @@ dfmc[dfmc.columns[-1]] = dfmc[dfmc.columns[-1]].round(2)
 # st.write(dfm.head(5))
 
 
-container = st.container(border=True)
-container.write("O gráfico abaixo mostra a distribuição da variável resposta por município. Permite visualizar Municípios com valores extremos e dispersão em torno da média.")
+
+
 st.markdown('Estatísticas')
 st.dataframe(dfmc[dfmc.columns[-1]].describe().to_frame().T)
 
 st.divider()
 
+st.markdown("O gráfico abaixo mostra a distribuição da variável resposta por município. Permite visualizar Municípios com valores extremos e dispersão em torno da média.")
 fig = px.scatter(
 dfmc.reset_index(),
 x="Município",
@@ -126,79 +139,19 @@ shapes=[
 
 # Show the scatterplot in Streamlit
 st.plotly_chart(fig, use_container_width=True)
-
-st.info(f'Variáveis por Município x {dfmc.columns[-1]}', icon='🌎')
-
-container = st.container(border=True)
-container.write("O gráfico abaixo mostra a relação da variável explicativa com a variável resposta. Permite visualizar como se correlacionam.")
-with st.expander('ajuda',expanded=False):
-    st.markdown('* $r = 1$:  correlação perfeita positiva, quanto maior o valor de uma variável, maior o valor da outra.')
-    st.markdown('* $r = 0$:  não há correlação, não importa o valor de uma variável, o valor da outra não é afetado.')
-    st.markdown('* $r = -1$:  correlação perfeita negativa, quanto maior o valor de uma variável, menor o valor da outra.')
-
-corr = dfm[dfm.columns[3:-1]].corrwith(dfm[df.columns[-1]]).sort_values(ascending=False)
-
-# Create a heatmap
-plt.figure(figsize=(10,8))
-sns.heatmap(corr.to_frame(), annot=True, cmap='coolwarm_r')
-
-# Show the heatmap in Streamlit
-st.pyplot(plt)
-
-variavel = st.selectbox('Selecione a variável', df.columns[3:-1])
-# Create a scatterplot of the penultimate column
-fig = px.scatter(
-dfm.reset_index(),
-x=variavel,
-y=dfmc.columns[-1],
-# size=dfmc.columns[-1],
-hover_name="Município",
-color=variavel,
-color_continuous_scale='icefire_r',
-)
-
-# Show the scatterplot in Streamlit
-st.plotly_chart(fig, use_container_width=True)
-
-st.info('Correlações por Municipio', icon='⚔️')
-
-#corr
-
-# Create a heatmap
-# fig = go.Figure(data=go.Heatmap(
-#                 x=corr.index,
-#                 y=['Correlation'],
-#                 z=[corr.values],
-#                 hoverongaps = False,
-#                 colorscale='RdBu'))
-
-# # Show the heatmap in Streamlit
-# st.plotly_chart(fig)
-
-
-with st.expander('Correlações por subunidade ⚔️',expanded=False):
-
-    # Calculate correlation
-    corr = df[df.columns[3:-1]].corrwith(df[df.columns[-1]]).sort_values(ascending=False)
-    # corr
-
-    # Create a heatmap
-    fig = go.Figure(data=go.Heatmap(
-                    z=corr.values,
-                    x=corr.index,
-                    y=['0'],
-                    hoverongaps = False,
-                    colorscale='Viridis'))
-
-    # Show the heatmap in Streamlit
-    st.plotly_chart(fig)
-
-    # Create a heatmap
-    plt.figure(figsize=(10,8))
-    sns.heatmap(corr.to_frame(), annot=True, cmap='coolwarm_r')
-
-    # Show the heatmap in Streamlit
-    st.pyplot(plt)
+st.info('Gráfico 1: gráfico de dispersão da distribuição das notas de proficiência dos estudantes por municípios')
+st.markdown(''' O gráfico 1 faz parte de uma análise estatística mais ampla apresentada no relatório, que visa explorar a variabilidade e o desempenho educacional em todo o estado. O gráfico 1 permite identificar visualmente quais municípios apresentam desempenhos extremos, tanto positivos quanto negativos, e como as notas estão dispersas em relação à média. Esta visualização facilita a identificação de áreas que necessitam de maior atenção e recursos.
+''')
+with st.container(border=True) as container2:
+    st.markdown(''' **Análise do Gráfico de Dispersão** \n
+    **Eixos do Gráfico** \n
+    -Eixo X: Representa os municípios de Pernambuco. Cada ponto ao longo deste eixo corresponde a um município específico.\n
+    -Eixo Y: Representa as notas de proficiência dos estudantes, variando de aproximadamente 0,43 a 0,70.\n
+    **Distribuição das Notas:** \n
+    -As diferentes cores dos pontos no gráfico indicam os níveis de proficiência, conforme a legenda de cores à direita.\n
+    -Cores Azuis: Indicam os municípios com as melhores notas de proficiência, situando-se entre 0,60 e 0,70.\n
+    -Cores Vermelhas: Indicam os municípios com as piores notas de proficiência, situando-se entre 0,43 e 0,50.
+    ''')
 
 st.subheader('Arvore de Decisão')
 # Define the features and the target
@@ -231,3 +184,17 @@ tree.plot_tree(reg, ax=ax, feature_names=X.columns, filled=True, fontsize=10)
 
 # Show the plot in Streamlit
 st.pyplot(fig)
+
+st.info('Figura 2: Árvore de Decisão')
+st.markdown(''' **Análise da Árvore de Decisão** \n
+A figura 2 apresenta uma análise com base em uma árvore de decisão, uma ferramenta de aprendizado de máquina usada para tomar decisões baseadas em dados. O objetivo da árvore de decisão é modelar e prever um valor de saída com base em diversas variáveis de entrada. \n
+Na parte superior da figura 2, temos uma tabela que lista a importância das variáveis utilizadas no modelo. A importância de uma variável é uma medida de quanto essa variável contribui para a predição do modelo. Essa tabela indica que a variável SAEPE - Participação é a mais importante para o modelo, seguida pela Adesão ao Programa Criança Alfabetizada e Adesão ao Compromisso Nacional Criança Alfabetizada. Outras variáveis: 0 (não tiveram contribuição significativa no modelo). 
+''')
+st.markdown(''' **Interpretação da Árvore de Decisão** \n
+A árvore de decisão está representada graficamente logo abaixo da tabela. Para interpretar a árvore, siga estas etapas:\n
+-Raiz da Árvore: A árvore começa com a variável mais importante, que é a SAEPE - Participação. O nó raiz divide os dados em duas partes com base em um valor de limiar para esta variável (<= 0.945).\n
+-Nódulos e Ramificações: Cada nó da árvore representa uma decisão baseada em um valor limiar de uma variável. Por exemplo, no segundo nível da árvore, para os dados que têm SAEPE - Participação menor ou igual a 0.945, a próxima divisão é baseada na Adesão ao Compromisso Nacional Criança Alfabetizada com um valor de corte de 0.5.\n
+-Folhas: As folhas da árvore (os nós finais) representam as predições finais do modelo. Cada folha mostra a média dos valores de saída dos exemplos que chegaram a esse nó, o erro quadrado médio (squared error), o número de amostras (samples), e o valor previsto (value).\n
+
+A árvore de decisão mostra que a SAEPE - Participação é a variável mais influente na predição do modelo, seguida pela Adesão ao Programa Criança Alfabetizada e Adesão ao Compromisso Nacional Criança Alfabetizada. As outras variáveis não tiveram impacto significativo nas previsões. A análise das subdivisões e folhas finais da árvore ajuda a entender como diferentes níveis de participação e adesão aos programas influenciam o valor previsto.
+''')
